76b160805bd214f40b1a776e2c2deaf32e7f00ca
Esmer Eshrati <esmer.eshrati@digigo.work>
2025-01-06 20:42:51 +0400

    about section is readyyy

--- js/translate.js
+++ js/translate.js
@@ -17,6 +17,62 @@ const translations = {
     slide1_text: "Hər yudumda mükəmməllik, hər fincanda zövq",
     slide2_text: "Təbiətin incəliyindən gələn dad",
     slide3_text: "Hər fincanda həyat enerjisi",
+    /////////// about//////////////////////
+    headerTitle: "BARSHOP – Barmenlər və Baristalar üçün Mağaza",
+    homeOptionsTitle1: "Ev və İaşə Obyektləri Üçün Uyğun:",
+    homeOptionsDescription1:
+      "Ev istifadəsi, ofis, Hotel və iaşə obyektləri üçün ideal həllər təqdim edirik. Peşəkar səviyyədə kofe hazırlamaq və kokteyl xidmətləri təqdim etmək istəyənlər üçün ən uyğun avadanlıq və aksesuarları bizdə tapın.",
+    homeOptionsTitle2: "Uygun Qiymətlər və Kampaniyalar:",
+    homeOptionsDescription2:
+      "Bizim məqsədimiz, keyfiyyətli məhsulları ən uyğun qiymətlərlə təqdim etməkdir. Geniş kampaniyalar, endirimlər və sərfəli təkliflər ilə sizin üçün daha əlverişli alış-veriş imkanı yaradırıq.",
+    headerDescription:
+      "Azərbaycanda kofe həvəskarlarının və peşəkarların seçimi! Biz, evdə rahat kofe hazırlamaq istəyənlərdən tutmuş peşəkar barista və barmenlərə qədər, hər kəs üçün yüksək keyfiyyətli məhsul və xidmətlər təqdim edirik.",
+    aboutTitle: "Barshop Haqqında",
+    aboutDescription1:
+      "Barshop olaraq, yeni başlayan bizneslər və peşəkarlar üçün ideal həllər təqdim edirik! Müştərilərimizə həm keyfiyyətli, həm də sərfəli qəhvə aparatları və aksessuarlar təqdim etməklə, onların işlərini bir addım irəliyə aparmaqda dəstək oluruq.",
+    aboutDescription2:
+      "Əgər qəhvə sənayesinə yeni daxil olmusunuzsa, biz sizin üçün mükəmməl başlanğıc nöqtəsiyik. Barista və barmenlər üçün xüsusi seçilmiş alətlər, geniş məhsul çeşidi və peşəkar xidmətlə işinizi asanlaşdırır və müştərilərinizə yüksək səviyyədə xidmət təqdim etməyinizə imkan yaradır.",
+    offerTitle: "Barshop-da Nə Tapacaqsınız?",
+    offerItem1: "Keyfiyyətli Məhsullar və Xidmətlər",
+    offerItem2: "Sərfəli Satış və İcarə Seçimləri",
+    offerItem3: "Ev və İaşə Obyektləri Üçün Ən Yaxşı Seçim",
+    productVarietyTitle: "Geniş Məhsul Seçimi",
+    rentalOptionsTitle: "Həm Satış, Həm İcarə Seçimləri",
+    productVarietyDescription:
+      "Bizim təklif etdiyimiz milkshake tozları, isti şokolad, salep, siroplar, chai tea və digər içki qarışıqları da seçimlərimiz arasındadır. İstədiyiniz hər şeyi bir məkanda tapa biləcəyiniz Barshop.",
+    rentalOptionsDescription:
+      "Yeni bizneslər üçün ilkin sərmayəni azaltmaq məqsədilə.İş yerlərində, tədbirlərdə və ictimai məkanlarda qəhvə mədəniyyəti getdikcə daha çox əhəmiyyət qazanır. Amma hər zaman yüksək keyfiyyətli qəhvə aparatı almaq sərfəli olmaya bilər. Bu problemin həlli üçün qəhvə aparatlarının icarəsi xidməti ideal seçimdir! Uygun şərtlər və müxtəlif ödəniş planları ilə həm peşəkar həm də yeni başlayan istifadəçilər üçün mükəmməl seçimlər təqdim edirik.",
+    whatWeOfferTitle: "Bütün Ehtiyaclarınıza Cavab Verən :",
+    coffeeMachinesTitle: "Kofe Aparatları",
+    coffeeMachinesDescription: "Espresso maşınları, filtre aparatları, kapsul və s.",
+    baristaAccessoriesTitle: "Barista Aksessuarları",
+    baristaAccessoriesDescription: "Tamperlər, pitcher-lər, barista tərəzisi və s.",
+    bartenderToolsTitle: "Barmen Alətləri",
+    bartenderToolsDescription: "Kokteyl shaker-ləri, qarışdırıcılar, ölçü fincanları və s.",
+    cocktailToolsTitle: "Kokteyl Alətləri",
+    cocktailToolsDescription: "Bütün içkilər üçün mükəmməl alətlər.",
+    contactUsTitle: "Bizimlə Əlaqə Saxlayın",
+    contactUsDescription:
+      "Siz də yüksək keyfiyyətli məhsullarla işinizi və ya kofe təcrübənizi mükəmməlləşdirin! İstər evinizdə kofe hazırlayın, istərsə də peşəkar iş mühitində müştərilərinizə mükəmməl xidmət təqdim edin, Barshop-da ehtiyacınız olan bütün kofe və barista alətləri mövcuddur. Bizimlə işinizi daha peşəkar, daha rahat və daha keyfiyyətli edin!",
+    deliveryAndPaymentTitle: "ÇATDIRILMA VƏ ÖDƏNİŞ",
+    deliveryTermsTitle: "Çatdırılma Şərtləri",
+    deliveryTerm1:
+      "Çatdırılma qiyməti malın qiymətinə daxil deyil və sifariş qəbul edildikdən sonra hesablanır. Hesablama seçilmiş çatdırılma xidmətinin tariflərinə əsaslanır.",
+    deliveryTerm2: "Bakı daxilində kuryerlə çatdırılma – 5 AZN.",
+    deliveryTerm3: "50 AZN-dən çox sifariş məbləği ilə Bakıda daxili pulsuz çatdırılma.",
+    deliveryTerm4:
+      "Bakıda sifarişlər saat 14:00-dan əvvəl növbəti iş günündə çatdırılır. Saat 14:00-dan sonra alınan sifarişlər bir iş günü sonra çatdırılır.",
+    deliveryTerm5: "Digər şəhərlərə çatdırılma Bakı Poçtu və ya digər xidmət vasitəsilə həyata keçirilir.",
+    deliveryTimeTitle: "Çatdırılma Müddəti",
+    deliveryTimeDescription:
+      "Bakıda çatdırılma müddəti müştərinin yerindən və sifarişin alınma vaxtından asılı olaraq 2 ilə 24 saat arasında dəyişir. Digər şəhərlərə çatdırılma fərdi şəkildə razılaşdırılır və seçilmiş xidmət və üsuldan asılıdır.",
+    paymentTitle: "Ödəniş",
+    paymentDescription:
+      "www.barshop.az saytından sifariş verdikdən sonra sifarişin ümumi dəyərini, o cümlədən çatdırılma və ödəniş üçün koordinatlarımızı qeyd etdiyiniz e-poçt ünvanına göndəririk.",
+    paymentMethodsTitle: "Ödəniş Yolları",
+    paymentMethod1: "Nağd pul.",
+    paymentMethod2: "Nağdsız ödənişlər (kart və ya hesabdan köçürmə ilə).",
+    paymentMethod3: "Bakı Poçtu və ya digər xidmət vasitəsilə mal göndərərkən ödəniş öncədən olunmalıdır.",
   },
   ru: {
     home: "ГЛАВНАЯ",
@@ -31,6 +87,62 @@ const translations = {
     slide1_text: "Совершенство в каждом глотке, радость в каждой чашке",
     slide2_text: "Вкус изысканности природы",
     slide3_text: "Энергия жизни в каждой чашке",
+    headerTitle: "BARSHOP – МАГАЗИН ДЛЯ БАРМЕНОВ И БАРИСТА",
+    headerDescription:
+      "Barshop – выбор любителей и профессионалов кофе в Азербайджане! Мы предоставляем высококачественные продукты и услуги для всех: от домашних пивоваров до профессиональных бариста и барменов.",
+    aboutTitle: "О Barshop",
+    aboutDescription1:
+      "Barshop предлагает идеальные решения для начинающих и профессиональных бизнесов! Мы предоставляем нашим клиентам качественные и доступные кофейные машины и аксессуары, поддерживая их в продвижении своего дела.",
+    aboutDescription2:
+      "Если вы только начинаете в кофейной индустрии, мы - идеальная отправная точка для вас. Инструменты для бариста и барменов, широкий ассортимент продукции и профессиональный сервис помогают упростить вашу работу и предоставить клиентам высококачественное обслуживание.",
+    offerTitle: "Что вы найдете в Barshop?",
+    offerItem1: "Качественные продукты и услуги",
+    offerItem2: "Выгодные варианты покупки и аренды",
+    offerItem3: "Лучший выбор для дома и заведений общепита",
+    ///// about//////////////////////
+    homeOptionsTitle1: "Идеально для дома и заведений:",
+    homeOptionsDescription1:
+      "Мы предлагаем идеальные решения для использования дома, в офисе, отелях и заведениях общепита. У нас вы найдете лучшее оборудование и аксессуары для приготовления кофе и коктейлей на профессиональном уровне.",
+    homeOptionsTitle2: "Доступные цены и акции:",
+    homeOptionsDescription2:
+      "Наша цель – предложить качественные продукты по самым выгодным ценам. Широкий выбор акций, скидок и выгодных предложений делает ваши покупки более доступными.",
+    productVarietyTitle: "Широкий выбор продукции: ",
+    rentalOptionsTitle: "Варианты продажи и аренды: ",
+    productVarietyDescription:
+      "В наш ассортимент входят порошки для молочных коктейлей, горячий шоколад, сиропы, чай и другие смеси напитков. Баршоп, где можно найти все, что пожелаешь, в одном месте.",
+    rentalOptionsDescription:
+      "для сокращения первоначальных инвестиций в новый бизнес.Культура кофе становится все более важной на рабочих местах, мероприятиях и в общественных местах. Но не всегда выгодно покупать качественную кофемашину. Для решения этой проблемы услуга аренды кофемашины – идеальный выбор! Благодаря доступным условиям и различным планам оплаты мы предоставляем отличные возможности как для профессиональных, так и для начинающих пользователей.",
+    whatWeOfferTitle: "Что мы предлагаем?",
+    coffeeMachinesTitle: "Кофейные машины",
+    coffeeMachinesDescription: "Эспрессо-машины, фильтры, капсулы и др.",
+    baristaAccessoriesTitle: "Аксессуары для бариста",
+    baristaAccessoriesDescription: "Темперы, питчеры, весы для бариста и др.",
+    bartenderToolsTitle: "Инструменты для барменов",
+    bartenderToolsDescription: "Шейкеры для коктейлей, мешалки, мерные стаканы и др.",
+    cocktailToolsTitle: "Инструменты для коктейлей",
+    cocktailToolsDescription: "Идеальные инструменты для всех напитков.",
+    contactUsTitle: "Свяжитесь с нами",
+    contactUsDescription:
+      "Сделайте свой бизнес или кофейный опыт совершенным с нашими высококачественными продуктами! Работайте с нами более профессионально, удобно и качественно!",
+    deliveryAndPaymentTitle: "ДОСТАВКА И ОПЛАТА",
+    deliveryTermsTitle: "Условия доставки",
+    deliveryTerm1:
+      "Стоимость доставки не включена в цену товара и рассчитывается после принятия заказа. Расчет производится на основе тарифов выбранной службы доставки.",
+    deliveryTerm2: "Доставка курьером по Баку — 5 AZN.",
+    deliveryTerm3: "Бесплатная доставка по Баку для заказов на сумму более 50 AZN.",
+    deliveryTerm4:
+      "Заказы по Баку, принятые до 14:00, доставляются на следующий рабочий день. Заказы, принятые после 14:00, доставляются через один рабочий день.",
+    deliveryTerm5: "Доставка в другие города осуществляется через Почту Баку или другие службы.",
+    deliveryTimeTitle: "Сроки доставки",
+    deliveryTimeDescription:
+      "Срок доставки по Баку варьируется от 2 до 24 часов в зависимости от местонахождения клиента и времени получения заказа. Доставка в другие города согласовывается индивидуально и зависит от выбранной службы и метода.",
+    paymentTitle: "Оплата",
+    paymentDescription:
+      "После оформления заказа на сайте www.barshop.az мы отправим общую стоимость заказа, включая доставку и платежные реквизиты, на указанный вами адрес электронной почты.",
+    paymentMethodsTitle: "Способы оплаты",
+    paymentMethod1: "Наличные.",
+    paymentMethod2: "Безналичные платежи (карта или банковский перевод).",
+    paymentMethod3: "При доставке товара через Почту Баку или другие службы оплата производится заранее.",
   },
   en: {
     home: "HOME",
@@ -45,6 +157,61 @@ const translations = {
     slide1_text: "Perfection in every sip, joy in every cup",
     slide2_text: "The taste from the delicacy of nature",
     slide3_text: "Life energy in every cup",
+    aboutTitle: "About Barshop",
+    homeOptionsTitle1: "Suitable for Home and Catering: ",
+    homeOptionsDescription1:
+      "We offer ideal solutions for home use, offices, hotels, and hospitality establishments. Find the best equipment and accessories for professional-level coffee preparation and cocktail services with us.",
+    homeOptionsTitle2: "Reasonable prices and promotions: ",
+    homeOptionsDescription2:
+      "Our goal is to provide high-quality products at the best prices. With extensive promotions, discounts, and advantageous offers, we make shopping more convenient for you.",
+    headerTitle: "BARSHOP – Store for Bartenders and Baristas",
+    headerDescription:
+      "Barshop is the choice of coffee lovers and professionals in Azerbaijan! We provide high-quality products and services for everyone: from home brewers to professional baristas and bartenders.",
+    aboutDescription1:
+      "At Barshop, we offer ideal solutions for startups and professionals! We support our customers by providing high-quality and affordable coffee machines and accessories to help take their business to the next level.",
+    aboutDescription2:
+      "If you are new to the coffee industry, we are the perfect starting point for you. With specially selected tools for baristas and bartenders, a wide range of products, and professional service, we simplify your work and enable you to provide top-notch service to your clients.",
+    offerTitle: "What Will You Find at Barshop?",
+    offerItem1: "Quality products and services",
+    offerItem2: "Affordable sales and rental options",
+    offerItem3: "The best choice for home and catering",
+    productVarietyTitle: "A wide range of products",
+    rentalOptionsTitle: "Sale and rental options",
+    productVarietyDescription:
+      "Our range includes milkshake powders, hot chocolate, syrups, tea and other drink mixes. A bar shop where you can find everything you want in one place.",
+    rentalOptionsDescription:
+      "To reduce the initial investment in a new business. Coffee culture is becoming increasingly important in workplaces, events and public spaces. But it is not always profitable to buy a quality coffee machine. To solve this problem, the coffee machine rental service is the perfect choice! With affordable terms and various payment plans, we provide excellent opportunities for both professional and novice users.",
+    whatWeOfferTitle: "Answering all your needs :",
+    coffeeMachinesTitle: "Coffee Machines",
+    coffeeMachinesDescription: "Espresso machines, filter machines, capsules, and more.",
+    baristaAccessoriesTitle: "Barista Accessories",
+    baristaAccessoriesDescription: "Tampers, pitchers, barista scales, and more.",
+    bartenderToolsTitle: "Bartender Tools",
+    bartenderToolsDescription: "Cocktail shakers, stirrers, measuring cups, and more.",
+    cocktailToolsTitle: "Cocktail Tools",
+    cocktailToolsDescription: "Perfect tools for all drinks.",
+    contactUsTitle: "Contact Us",
+    contactUsDescription:
+      "Improve your work or coffee with high-quality products! Whether you brew coffee at home or provide your customers with excellent service in a professional business environment, Barshop has all the necessary coffee and barista tools. Make your work more professional, convenient and better with us!",
+    deliveryAndPaymentTitle: "DELIVERY AND PAYMENT",
+    deliveryTermsTitle: "Delivery Terms",
+    deliveryTerm1:
+      "The delivery cost is not included in the product price and is calculated after the order is accepted. The calculation is based on the rates of the selected delivery service.",
+    deliveryTerm2: "Courier delivery within Baku – 5 AZN.",
+    deliveryTerm3: " For orders over 50 AZN - Free delivery in Baku",
+    deliveryTerm4:
+      "Orders in Baku are delivered the next business day before 14:00. We do not guarantee next day delivery if orders are received after 14:00. In this case, delivery is carried out one business day after receiving the order.",
+    deliveryTerm5: "Delivery to other cities is carried out via Baku Post or another service. Delivery conditions",
+    deliveryTimeTitle: "Delivery Time",
+    deliveryTimeDescription:
+      "Delivery time in Baku varies from 2 to 24 hours, depending on the location of the customer and the time of receipt of the order.",
+    paymentTitle: "Payment",
+    paymentDescription:
+      "After placing an order on www.barshop.az, we will send the total cost of the order, including delivery and payment details, to the email address you provided.Orders are delivered to the address you specify (be careful when filling out the personal information and address form)",
+    paymentMethodsTitle: "Payment Methods",
+    paymentMethod1: "Cash",
+    paymentMethod2: "Cashless payment (Cart) (by transfer from an account)",
+    paymentMethod3: "When sending goods via Baku Post or another service, payment must be made in advance",
   },
 };
 const lang1 = document.querySelector("#lang");
